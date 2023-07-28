cee2da1aec85c3c2164f1d8830f4869a015f7aa5
John Barton <johnrbarton27@gmail.com>
2023-07-27 21:23:58 -0400

    Add automatic strikethrough when checklist item is completed

--- streamstatus/static/js/checklist.js
+++ streamstatus/static/js/checklist.js
@@ -2,6 +2,14 @@ function updateCheckboxStatus(checkbox) {
     const checkboxId = checkbox.id;
     const isChecked = checkbox.checked;
 
+    const labelElem = document.getElementById(checkboxId + "_LABEL")
+
+    if (isChecked) {
+        labelElem.classList.add("strikethrough")
+    } else {
+        labelElem.classList.remove("strikethrough")
+    }
+
     // Send the API request to localhost:8001/api/update_checkbox_status
     // Replace this URL with your actual server endpoint
     const apiUrl = 'http://localhost:8001/api/update_checkbox_status';
